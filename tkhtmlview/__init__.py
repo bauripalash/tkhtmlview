24e8f7c1da980e1749eef31f32b8ccc7a4752221
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-03-30 22:34:30 +0530

    Fix, add feature support...  - Remove unnecessary imports.  - Bump version.  - Add support for RenderHTML.  - Run code python formatter.

--- tkhtmlview/__init__.py
+++ tkhtmlview/__init__.py
@@ -3,13 +3,14 @@ tkinter HTML text widgets
 """
 import sys
 import tkinter as tk
-from tkinter import scrolledtext
-from tkinter import font
 from tkhtmlview import html_parser
+from tkhtmlview.utils import RenderHTML
+
+VERSION = "0.1.0.post2"
 
 
 class _ScrolledText(tk.Text):
-    #----------------------------------------------------------------------------------------------
+
     def __init__(self, master=None, **kw):
         self.frame = tk.Frame(master)
 
@@ -20,9 +21,10 @@ class _ScrolledText(tk.Text):
 
         tk.Text.__init__(self, self.frame, **kw)
         self.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
+
         text_meths = vars(tk.Text).keys()
-        methods = vars(tk.Pack).keys() | vars(tk.Grid).keys() | vars(tk.Place).keys()
+        methods = vars(tk.Pack).keys() | vars(
+            tk.Grid).keys() | vars(tk.Place).keys()
         methods = methods.difference(text_meths)
 
         for m in methods:
@@ -32,22 +34,23 @@ class _ScrolledText(tk.Text):
     def __str__(self):
         return str(self.frame)
 
+
 class HTMLScrolledText(_ScrolledText):
-    #----------------------------------------------------------------------------------------------
+
     """
     HTML scrolled text widget
     """
+
     def __init__(self, *args, html=None, **kwargs):
-        #------------------------------------------------------------------------------------------
         super().__init__(*args, **kwargs)
         self._w_init(kwargs)
         self.html_parser = html_parser.HTMLTextParser()
         if isinstance(html, str):
             self.set_html(html)
-
+        elif isinstance(html, RenderHTML):
+            self.set_html(html.get_html())
 
     def _w_init(self, kwargs):
-        #------------------------------------------------------------------------------------------
         if not 'wrap' in kwargs.keys():
             self.config(wrap='word')
         if not 'background' in kwargs.keys():
@@ -56,9 +59,7 @@ class HTMLScrolledText(_ScrolledText):
             else:
                 self.config(background='white')
 
-
     def fit_height(self):
-        #------------------------------------------------------------------------------------------
         """
         Fit widget height to wrapped lines
         """
@@ -70,9 +71,7 @@ class HTMLScrolledText(_ScrolledText):
         else:
             self.config(height=0.5+3/self.yview()[1])
 
-
     def set_html(self, html, strip=True):
-        #------------------------------------------------------------------------------------------
         """
         Set HTML widget text. If strip is enabled (default) it ignores spaces and new lines.
 
@@ -86,43 +85,41 @@ class HTMLScrolledText(_ScrolledText):
 
 
 class HTMLText(HTMLScrolledText):
-    #----------------------------------------------------------------------------------------------
+
     """
     HTML text widget
     """
+
     def _w_init(self, kwargs):
-        #------------------------------------------------------------------------------------------
         super()._w_init(kwargs)
         self.vbar.pack_forget()
 
     def fit_height(self):
-        #------------------------------------------------------------------------------------------
         super().fit_height()
-        #self.master.update()
+        # self.master.update()
         self.vbar.pack_forget()
 
+
 class HTMLLabel(HTMLText):
-    #----------------------------------------------------------------------------------------------
+
     """
     HTML label widget
     """
+
     def _w_init(self, kwargs):
-        #------------------------------------------------------------------------------------------
         super()._w_init(kwargs)
         if not 'background' in kwargs.keys():
             if sys.platform.startswith('win'):
                 self.config(background='SystemButtonFace')
             else:
                 self.config(background='#d9d9d9')
-                
+
         if not 'borderwidth' in kwargs.keys():
             self.config(borderwidth=0)
 
         if not 'padx' in kwargs.keys():
             self.config(padx=3)
-        
+
     def set_html(self, *args, **kwargs):
-        #------------------------------------------------------------------------------------------
         super().set_html(*args, **kwargs)
         self.config(state=tk.DISABLED)
-
