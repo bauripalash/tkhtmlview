0dbea50c97a5d62355815c663c007a1038f9cc14
Palash Bauri <palashbauri1@gmail.com>
2022-08-26 22:02:00 +0530

    updated to version 0.1.1; added some more info in package

--- setup.py
+++ setup.py
@@ -8,17 +8,33 @@ setuptools.setup(
     name="tkhtmlview",
     version=VERSION,
     author="Palash Bauri",
-    author_email="hey@palashbauri.in",
+    keywords='tkinter html developement webview'
     description="View Simple HTML docs on tkinter",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bauripalash/tkhtmlview",
+
+    project_urls={
+        'Documentation': 'https://github.com/bauripalash/tkhtmlview',
+        'Funding': 'https://github.com/bauripalash',
+        'Source': 'https://github.com/bauripalash/tkhtmlview',
+        'Tracker': 'https://github.com/bauripalash/tkhtmlview/issues',
+    },
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+
+
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires=">=3.4.*",
     install_requires=['Pillow>=5.3.0','requests>=2.22.0'],
