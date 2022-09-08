39dee85a689a6b7eabf0ade95bcdfb00f1ea5326
Palash Bauri <palashbauri1@gmail.com>
2022-09-08 11:25:50 +0530

    Version bump 0.1.1.post2

--- tkhtmlview/__init__.py
+++ tkhtmlview/__init__.py
@@ -6,7 +6,7 @@ import tkinter as tk
 from tkhtmlview import html_parser
 from tkhtmlview.utils import RenderHTML
 
-VERSION = "0.1.1.post1"
+VERSION = "0.1.1.post2"
 
 class _ScrolledText(tk.Text):
 
--- version.py
+++ version.py
@@ -1,1 +1,1 @@
-VERSION = "0.1.1.post1"
+VERSION = "0.1.1.post2"
