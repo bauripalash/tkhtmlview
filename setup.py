141f772bf82750e24c088c911f111ebc713ac03a
Palash Bauri <palashbauri1@gmail.com>
2025-10-01 10:26:03 +0530

    update poetry version, requests, python versions
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=["Pillow>=10", "requests>=2.22.0"],
 )
